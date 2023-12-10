7fd6724a56f3d351226b7b0d04b4f182822fd448
take <takehiro.arai.jg@gmail.com>
2023-12-10 15:08:19 +0900

    end

--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -7,20 +7,21 @@ const insertPost = async () => {
 
 const Post = () => {
     return(
-        <div className="h-full grid grid-cols-3 gap-4 place-items-center h-4/5">
-            <IconList src="/images/post1.png" alt="icon1" url="" />
-            <IconList src="/images/post2.png" alt="icon2" url="" />
-            <IconList src="/images/post3.png" alt="icon3" url="" />
-            <IconList src="/images/post4.png" alt="icon4" url="" />
-            <IconList src="/images/post5.png" alt="icon5" url="" />
-            <IconList src="/images/post6.png" alt="icon6" url="" />
-            <IconList src="/images/post7.png" alt="icon7" url="" />
-            <IconList src="/images/post8.png" alt="icon8" url="" />
-            <IconList src="/images/post9.png" alt="icon9" url="" />
-            <IconList src="/images/post10.png" alt="icon10" url="" />
-
-            <button onClick={() => insertPost()}>Postする</button>
+        <div>
+            <div className="h-full grid grid-cols-3 gap-4 place-items-center h-4/5">
+                <IconList src="/images/post1.png" alt="津波" url="a" />
+                <IconList src="/images/post2.png" alt="ミサイル" url="a" />
+                <IconList src="/images/post3.png" alt="地震" url="a" />
+                <IconList src="/images/post4.png" alt="通行止め" url="a" />
+                <IconList src="/images/post5.png" alt="渋滞" url="a" />
+                <IconList src="/images/post6.png" alt="事故" url="a" />
+                <IconList src="/images/post7.png" alt="危険" url="a" />
+                <IconList src="/images/post8.png" alt="台風" url="a" />
+                <IconList src="/images/post9.png" alt="米軍基地" url="a" /> 
+            </div>
+            <div className="text-center"><IconList src="/images/post10.png" alt="有事（台湾・中国・北朝鮮）" url="a"  /></div>
         </div>
+        
     )
 }
 
